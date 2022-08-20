987a29b727e75761751895226370f470fd4342bc
Aliyu abdulrazak <aliyuabdulrazaks539@gmail.com>
2022-08-20 04:07:52 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+
+import React, {useState, useEffect } from 'react';
 import './App.css';
+const API_URL = 'http://api.icndb.com/jokes/random'
 
 function App() {
+  const[joke, setJoke] = useState('')
+  
+  const generateJoke = ()=>{
+    fetch(API_URL)
+    .then(res => res.json())
+    .then(data => setJoke(data.value.joke))
+  }
+  useEffect(()=>{
+    generateJoke()
+  },[])
   return (
+    <>
+   <div className='top'><h1><i>laugh away ur sorrows</i></h1></div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='card'>
+      <h1 className='heading'>Razak jokes</h1>
+        <p><span><h1>crack ur ribs!!!</h1></span></p>
+        <p><h1>{joke}</h1></p>
+        <button onClick={generateJoke}><h3>get new jokes</h3></button>
+      </div>
     </div>
+    </>
   );
 }
 
